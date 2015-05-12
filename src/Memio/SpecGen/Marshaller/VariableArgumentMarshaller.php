c9c137a9055568c867354ab24c4baa41dae835c4
Loïc Chardonnet <loic.chardonnet@gmail.com>
2015-05-12 18:49:44 +0100

    Fixed Prophecy test double type guessing

--- src/Memio/SpecGen/Marshaller/VariableArgumentMarshaller.php
+++ src/Memio/SpecGen/Marshaller/VariableArgumentMarshaller.php
@@ -3,6 +3,7 @@
 namespace Memio\SpecGen\Marshaller;
 
 use Memio\SpecGen\Marshaller\Model\ArgumentCollection;
+use Prophecy\Prophecy\ProphecySubjectInterface;
 
 class VariableArgumentMarshaller
 {
@@ -19,11 +20,48 @@ class VariableArgumentMarshaller
             $name = 'argument';
             if (is_object($variable)) {
                 $type = get_class($variable);
+                if ($variable instanceof ProphecySubjectInterface) {
+                    $type = ($this->getProphecyBaseType($variable));
+                }
                 $name = lcfirst(end(explode('\\', $type)));
             }
             $argumentCollection->add($type, $name);
         }
 
         return $argumentCollection->all();
+    }
+
+    /**
+     * @param mixed $variable
+     *
+     * @return mixed|string
+     */
+    private function getProphecyBaseType($variable)
+    {
+        $typeName = get_parent_class($variable);
+
+        if ($typeName == 'stdClass'
+            && $interfaces = $this->getAllInterfaces($variable)) {
+            $typeName = reset($interfaces);
+        }
+
+        return $typeName;
+    }
+
+    /**
+     * @param mixed $variable
+     *
+     * @return array
+     */
+    private function getAllInterfaces($variable)
+    {
+        $interfaces = array_filter(
+            class_implements($variable),
+            function ($el) {
+                return 0 !== strpos($el, 'Prophecy\\');
+            }
+        );
+
+        return $interfaces;
     }
 }
