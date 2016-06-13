499cb3f751f191499353cb735fde96c9a6cfb013
Loïc Faugeron <faugeron.loic@gmail.com>
2016-06-13 17:01:39 +0100

    Website generation with Couscous

--- bin/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-composer --quiet install --optimize-autoloader
-
-vendor/bin/phpspec --no-interaction run --format=dot
-vendor/bin/phpunit
-vendor/bin/php-cs-fixer fix --dry-run src
-vendor/bin/php-cs-fixer fix --dry-run spec --fixers=-visibility
-vendor/bin/php-cs-fixer fix --dry-run tests
